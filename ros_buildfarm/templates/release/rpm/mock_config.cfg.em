cottsay/ros_buildfarm

commit 1b7c4ce2872c0ea98540492195dcb4d16a29d172

Change --env-vars to parse as a dict (#760)
In the templates, `env_vars` is now a dictionary, but
`build_environment_variables` remains as a list.

This change gives us earlier feedback when invalid values are passed to
`--env-vars`, and gives us greater flexibility in what we do with the
passed environment variables.

ros_buildfarm/templates/release/rpm/mock_config.cfg.em
@@ -13,7 +13,7 @@ config_opts['microdnf_builddep_opts'] = config_opts.get('microdnf_builddep_opts'
 
 @[if build_environment_variables]@
 # Set environment vars from the build config
-@[for env_key, env_val in [env_var.split('=', 1) for env_var in build_environment_variables]]@
+@[for env_key, env_val in env_vars]@
 config_opts['environment']['@env_key'] = '@env_val'
 @[end for]
 @[end if]@

ros_buildfarm/argument.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import argparse
+from collections import OrderedDict
 import os
 
 
@@ -67,8 +68,8 @@ def add_argument_build_name(parser, build_file_type, nargs=None):
 
 def add_argument_env_vars(parser):
     parser.add_argument(
-        '--env-vars',
-        nargs='*', default=[],
+        '--env-vars', action=OrderedDictAction,
+        nargs='*', default=OrderedDict(),
         help="Environment variables as 'key=value' for Dockerfile "
              'ENV directives')
 
@@ -469,3 +470,16 @@ def extract_multiple_remainders(argv, arguments):
             remainders[argument.dest] = argv[index + 1:]
             del argv[index:]
     return remainders
+
+
+class OrderedDictAction(argparse.Action):
+    def __call__(self, parser, args, values, option_string=None):
+        dest = OrderedDict()
+        for value in values:
+            value_parts = value.split('=', 1)
+            if len(value_parts) != 2:
+                raise argparse.ArgumentError(
+                    argument=self,
+                    message="invalid key/value pair: '%s'" % value)
+            dest[value_parts[0].strip()] = value_parts[1].strip()
+        setattr(args, self.dest, dest)

ros_buildfarm/templates/ci/ci_create_tasks.Dockerfile.em
@@ -59,7 +59,7 @@ args = \
     ' --workspace-root ' + ' '.join(workspace_mount_point) + \
     ' --distribution-repository-urls ' + ' '.join(distribution_repository_urls) + \
     ' --distribution-repository-key-files ' + ' ' .join(['/tmp/keys/%d.key' % i for i in range(len(distribution_repository_keys))]) + \
-    ' --env-vars ' + ' ' .join(env_vars)
+    ' --env-vars ' + ' ' .join(['%s=%s' % key_value for key_value in env_vars.items()])
 build_args = args + \
     ' --build-tool ' + build_tool + \
     ' --ros-version ' + str(ros_version) + \

ros_buildfarm/templates/devel/devel_create_tasks.Dockerfile.em
@@ -82,7 +82,7 @@ cmd = \
     ' --distribution-repository-key-files ' + ' ' .join(['/tmp/keys/%d.key' % i for i in range(len(distribution_repository_keys))]) + \
     ' --build-tool ' + build_tool + \
     ' --ros-version ' + str(ros_version) + \
-    ' --env-vars ' + ' ' .join(env_vars)
+    ' --env-vars ' + ' ' .join(['%s=%s' % key_value for key_value in env_vars.items()])
 if run_abichecker:
     cmd += ' --run-abichecker'
 if require_gpu_support:

scripts/ci/build_task_generator.py
@@ -117,7 +117,7 @@ def main(argv=sys.argv[1:]):
         'build_tool_args': args.build_tool_args,
         'ros_version': args.ros_version,
 
-        'build_environment_variables': args.env_vars,
+        'build_environment_variables': ['%s=%s' % key_value for key_value in args.env_vars.items()],
 
         'install_lists': install_lists,
         'dependencies': [],

scripts/ci/create_workspace_task_generator.py
@@ -99,7 +99,7 @@ def main(argv=sys.argv[1:]):
 
         'uid': get_user_id(),
 
-        'build_environment_variables': args.env_vars,
+        'build_environment_variables': ['%s=%s' % key_value for key_value in args.env_vars.items()],
 
         'dependencies': debian_pkg_names,
         'dependency_versions': debian_pkg_versions,

scripts/devel/create_devel_task_generator.py
@@ -77,11 +77,7 @@ def main(argv=sys.argv[1:]):
              'and instead of installing the tests are ran')
     args = parser.parse_args(argv)
 
-    condition_context = {}
-    for t in args.env_vars:
-        parts = t.split('=', 1)
-        assert len(parts) == 2, '--env-vars argument lacks equal sign: ' + t
-        condition_context[parts[0]] = parts[1]
+    condition_context = dict(args.env_vars)
     condition_context['ROS_DISTRO'] = args.rosdistro_name
     condition_context['ROS_VERSION'] = args.ros_version
 
@@ -175,7 +171,7 @@ def main(argv=sys.argv[1:]):
         'build_tool': args.build_tool,
         'ros_version': args.ros_version,
 
-        'build_environment_variables': args.env_vars,
+        'build_environment_variables': ['%s=%s' % key_value for key_value in args.env_vars.items()],
 
         'dependencies': debian_pkg_names,
         'dependency_versions': debian_pkg_versions,

scripts/release/create_binarydeb_task_generator.py
@@ -97,7 +97,7 @@ def main(argv=sys.argv[1:]):
             args.distribution_repository_urls,
             args.distribution_repository_key_files),
 
-        'build_environment_variables': args.env_vars,
+        'build_environment_variables': ['%s=%s' % key_value for key_value in args.env_vars.items()],
 
         'dependencies': debian_pkg_names,
         'dependency_versions': debian_pkg_versions,

commit a9f0363ac9b14480227a8d33ea4e49106933a46c

Fix a regression in RPM jobs
Regression caused by 1b7c4ce2872c0ea98540492195dcb4d16a29d172

ros_buildfarm/templates/release/rpm/mock_config.cfg.em
@@ -11,7 +11,7 @@ config_opts['yum_builddep_opts'] = config_opts.get('yum_builddep_opts', []) + ['
 config_opts['dnf_builddep_opts'] = config_opts.get('dnf_builddep_opts', []) + ['--setopt=install_weak_deps=True']
 config_opts['microdnf_builddep_opts'] = config_opts.get('microdnf_builddep_opts', []) + ['--setopt=install_weak_deps=True']
 
-@[if build_environment_variables]@
+@[if env_vars]@
 # Set environment vars from the build config
 @[for env_key, env_val in env_vars]@
 config_opts['environment']['@env_key'] = '@env_val'